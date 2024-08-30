255bf915c811ac7a28312330b97a79be4cabfac6
Baptiste Bessard <bessardbusiness@gmail.com>
2024-08-30 16:43:43 +0200

    getters, setters, main to help parsing

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,9 +1,33 @@
 #include <string>
-#include <iostream> 
+#include <iostream
+#include "HttpRequest.hpp"
+
 
 int main(int ac, char **av) {
     if (ac != 2) {
         std::cout << "Binary must be: ./webserv [configuration file]" << std::endl;
         return (1);
     }
+
+    HttpRequest simulatedRequest;
+
+    // Utilisation des setters pour initialiser les valeurs
+    simulatedRequest.setMethod("GET");
+    simulatedRequest.setURI("/index.html");
+    simulatedRequest.setHTTPVersion("HTTP/1.1");
+
+    std::map<std::string, std::string> headers;
+    headers["Host"] = "localhost";
+    headers["Connection"] = "keep-alive";
+    simulatedRequest.setHeaders(headers);
+
+    simulatedRequest.setBody("");
+    simulatedRequest.setIsChunked(false);
+
+    // Utilisation des getters pour récupérer les valeurs (exemple)
+    std::cout << "Method: " << simulatedRequest.getMethod() << std::endl;
+    std::cout << "URI: " << simulatedRequest.getURI() << std::endl;
+    std::cout << "HTTP Version: " << simulatedRequest.getHTTPVersion() << std::endl;
+
+    return 0;
 }
