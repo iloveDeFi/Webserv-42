563f9449838a372fc2ba50b03067c5a40e0fe695
helog234 <helenegandar@ik.me>
2024-09-18 17:31:58 +0200

    merged main from httprequest

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <iostream>
-<<<<<<< HEAD
 #include <fstream>
 #include "Config.hpp"
 #include "Location.hpp"
@@ -8,10 +7,6 @@
 #include "Socket.hpp"
 #include "Client.hpp"
 #include "Utils.hpp"
-=======
-#include "HttpRequest.hpp"
-
->>>>>>> feature/http-request
 
 int main(int ac, char **av)
 {
@@ -19,15 +14,6 @@ int main(int ac, char **av)
         std::cout << "Binary must be: ./webserv [configuration file]" << std::endl;
         return (1);
     }
-<<<<<<< HEAD
-	const size_t buffer_size = 1024;
-	ifstream settings(argv[1].c_str());
-	Config config(settings);
-	Server webserv(config.getFdConfig(), config.getLocation());
-	webserv.handleRequest();
-	return (0);
-}
-=======
 
     HttpRequest simulatedRequest;
 
@@ -48,4 +34,3 @@ int main(int ac, char **av)
 
     return 0;
 }
->>>>>>> feature/http-request
