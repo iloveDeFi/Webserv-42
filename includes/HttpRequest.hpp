d9d37d99b3056c1acef715014873888e406e3dcd
astutz <astutz@puresante.fr>
2024-09-18 16:04:58 +0200

    parsing request done

---
+++ includes/HttpRequest.hpp
@@ -0,0 +1,74 @@
+#ifndef HTTPREQUEST_HPP
+# define HTTPREQUEST_HPP
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include "HttpRequestException.hpp"
+
+const size_t MAX_BODY_SIZE = 2 * 1024 * 1024; // 2 MB
+
+class HttpRequest {
+    private:
+        std::string _method;
+        std::string _uri;
+        std::string _version;
+        std::map<std::string, std::string> _headers;
+        std::string _body;
+        std::map<std::string, std::string> _queryParameters;
+        bool _isChunked;
+        std::set<std::string> _allowedMethods;
+
+        static const std::set<std::string> initMethods();
+
+        // Méthodes privées pour extraire les parties de la requête
+        std::string extractMethod(const std::string& raw_request);
+        std::string extractURI(const std::string& raw_request);
+        std::map<std::string, std::string> extractHeaders(const std::string& raw_request);
+        std::string extractBody(const std::string& raw_request);
+        std::string extractHTTPVersion(const std::string& raw_request);
+        std::map<std::string, std::string> extractQueryParameters(const std::string& uri);
+        bool checkIfChunked(const std::string& raw_request);
+
+        // Fonctions auxiliaires
+        std::string trim(const std::string& str);
+        std::string safe_substr(const std::string& str, size_t start, size_t length);
+        bool hasBody(const std::string& raw_request);
+        bool isValidRequest(const std::string& raw_request);
+        void clearRequestData();
+
+    public:
+        HttpRequest();
+        ~HttpRequest();
+        HttpRequest(const HttpRequest& src);
+        HttpRequest& operator=(const HttpRequest& src);
+
+        // Méthode de parsing principale
+        void parse(const std::string& raw_request);
+		void testRequest(const std::string& raw_request);
+
+        // Getters
+        std::string getMethod() const;
+        std::string getURI() const;
+        std::string getHTTPVersion() const;
+        std::map<std::string, std::string> getHeaders() const;
+		std::string getHeader(const std::string& name) const;
+        std::string getBody() const;
+		std::map<std::string, std::string> getQueryParameters() const;
+        bool isChunked() const;
+
+		// Setters
+		void setMethod(const std::string& method);
+		void setURI(const std::string& uri);
+		void setHTTPVersion(const std::string& version);
+		void setHeaders(const std::map<std::string, std::string>& headers);
+		void setBody(const std::string& body);
+		void setIsChunked(bool isChunked);
+		void setQueryParameters(const std::map<std::string, std::string>& queryParameters);
+};
+
+// Fonction pour imprimer les détails de la requête
+std::ostream& operator<<(std::ostream& os, const HttpRequest& req);
+
+#endif
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,9 +1,82 @@
+#include <iostream>
 #include <string>
-#include <iostream> 
+#include "../includes/HttpRequest.hpp"
 
-int main(int ac, char **av) {
-    if (ac != 2) {
-        std::cout << "Binary must be: ./webserv [configuration file]" << std::endl;
-        return (1);
-    }
+void testRequest(const std::string& raw_request);
+
+int main() {
+    // Test des requêtes
+    std::string raw_request_1 = "POST /api/v1/upload HTTP/1.1\r\n"
+                                 "Host: www.example.com\r\n"
+                                 "Content-Type: application/json\r\n"
+                                 "Content-Length: 27\r\n"
+                                 "Authorization: Bearer your_access_token_here\r\n"
+                                 "User-Agent: Mozilla/5.0\r\n"
+                                 "\r\n"
+                                 "{\"name\": \"example\", \"type\": \"test\"}\r\n";
+
+    std::string raw_request_2 = "POST /upload HTTP/1.1\r\n"
+                                 "Host: upload.example.com\r\n"
+                                 "Transfer-Encoding: chunked\r\n"
+                                 "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                                 "User-Agent: Mozilla/5.0\r\n"
+                                 "\r\n"
+                                 "7\r\n"
+                                 "file1=\r\n"
+                                 "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                                 "Content-Disposition: form-data; name=\"file1\"; filename=\"example.txt\"\r\n"
+                                 "Content-Type: text/plain\r\n"
+                                 "\r\n"
+                                 "This is the content of the file.\r\n"
+                                 "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                                 "Content-Disposition: form-data; name=\"file2\"; filename=\"example2.txt\"\r\n"
+                                 "Content-Type: text/plain\r\n"
+                                 "\r\n"
+                                 "Another file content.\r\n"
+                                 "0\r\n"
+                                 "\r\n";
+
+    std::string raw_request_3 = "POST /api/v1/upload\r\n"
+                                 "Host: www.example.com\r\n"
+                                 "Content-Type: application/json\r\n"
+                                 "Content-Length: 27\r\n"
+                                 "Authorization: Bearer your_access_token_here\r\n"
+                                 "User-Agent: Mozilla/5.0\r\n"
+                                 "\r\n"
+                                 "{\"name\": \"example\", \"type\": \"test\"}\r\n";
+
+    std::string raw_request_4 = "POST /upload HTTP/1.1\r\n"
+                                 "Host: upload.example.com\r\n"
+                                 "Transfer-Encoding chunked\r\n"  // Erreur: manque les deux-points
+                                 "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                                 "User-Agent: Mozilla/5.0\r\n"
+                                 "\r\n"
+                                 "7\r\n"
+                                 "file1=\r\n"
+                                 "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                                 "Content-Disposition: form-data; name=\"file1\"; filename=\"example.txt\"\r\n"
+                                 "Content-Type: text/plain\r\n"
+                                 "\r\n"
+                                 "This is the content of the file.\r\n"
+                                 "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+                                 "Content-Disposition: form-data; name=\"file2\"; filename=\"example2.txt\"\r\n"
+                                 "Content-Type: text/plain\r\n"
+                                 "\r\n"
+                                 "Another file content.\r\n"
+                                 "0\r\n"
+                                 "\r\n";
+
+    std::cout << "Testing raw_request_1:\n";
+    testRequest(raw_request_1);
+
+    std::cout << "\nTesting raw_request_2:\n";
+    testRequest(raw_request_2);
+
+    std::cout << "\nTesting raw_request_3:\n";
+    testRequest(raw_request_3);
+
+    std::cout << "\nTesting raw_request_4:\n";
+    testRequest(raw_request_4);
+
+    return 0;
 }
