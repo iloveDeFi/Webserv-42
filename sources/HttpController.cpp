16bff8213b3a695f47667833a0b98bd0aa9252d3
Baptiste Bessard <bessardbusiness@gmail.com>
2024-10-30 12:19:14 +0100

    last commit

--- sources/HttpController.cpp
+++ sources/HttpController.cpp
@@ -422,7 +422,7 @@ void RequestController::handleUnknownResponse(const HttpRequest &req, HttpRespon
         res.generate400BadRequest("400 Bad Request: Method cannot be empty.");
         return;
     }
-    res.generate405MethodNotAllowed("405: Method not allowred: " + method);
+    res.generate405MethodNotAllowed("405: Method not allowed: " + method);
     // res.generate501NotImplemented("501 Not Implemented: The server does not recognize the HTTP method used: " + method);
     res.setHTTPVersion(version);
 }
